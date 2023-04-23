0075865323edee8d071070dd62a68ea0d732fa93
Apiyo4 <odingoannette@gmail.com>
2023-04-23 11:36:43 -0400

    increase request size

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 const port = process.env.PORT;
 connectDB();
 const app = express();
+app.use(express.json({ limit: '50mb' })); 
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
